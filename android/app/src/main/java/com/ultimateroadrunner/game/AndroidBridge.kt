1c82cc811c29e150b11744d1a33cbeea36e2ff08
nwalin480-arch <nwalin480@gmail.com>
2026-06-22 20:19:00 +0100

    Fix package name to com.ultimateroadrunner.game (AndroidBridge)

---
+++ android/app/src/main/java/com/ultimateroadrunner/game/AndroidBridge.kt
@@ -0,0 +1,43 @@
+package com.ultimateroadrunner.game
+
+import android.content.Intent
+import android.net.Uri
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+
+class AndroidBridge(
+    private val activity: MainActivity,
+    private val webView: WebView
+) {
+
+    @JavascriptInterface
+    fun showInterstitialAd() { activity.showInterstitialAd() }
+
+    @JavascriptInterface
+    fun showRewardedAd() { activity.showRewardedAd() }
+
+    @JavascriptInterface
+    fun showBanner() { activity.showAdBanner() }
+
+    @JavascriptInterface
+    fun hideBanner() { activity.hideAdBanner() }
+
+    @JavascriptInterface
+    fun openPrivacyPolicy() { activity.openPrivacyPolicy() }
+
+    @JavascriptInterface
+    fun onGameReady() {
+        android.util.Log.d("AndroidBridge", "Game is ready")
+    }
+
+    @JavascriptInterface
+    fun getLanguage(): String {
+        return activity.resources.configuration.locales[0].language
+    }
+
+    fun callJS(functionName: String) {
+        webView.post {
+            webView.evaluateJavascript("window.$functionName && window.$functionName()", null)
+        }
+    }
+}
